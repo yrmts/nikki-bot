1052d503d789685a1c013d020be627c91eff1a41
Yuri Matsui <yurimatsui37@gmail.com>
2016-04-29 16:10:46 +0900

    move callback to verify

--- app/controllers/api/fbbot_controller.rb
+++ app/controllers/api/fbbot_controller.rb
@@ -1,12 +1,12 @@
 class Api::FbbotController < ApplicationController
   def callback
+  end
+
+  def verify
     if params['hub_verify_token'] === 'LEPrecon37'
       render text: params['hub.challenge']
     else
       render nothing: true, status: ok
     end
-  end
-
-  def verify
   end
 end
